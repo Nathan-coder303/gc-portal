cab45393c3247b2171bda944a7dd7d1a31a4e3cb
mike <mike@mikes-Mac-mini.local>
2026-05-07 05:52:57 -0400

    Remove unused Image import in MobileProjectList

--- src/components/projects/MobileProjectList.tsx
+++ src/components/projects/MobileProjectList.tsx
@@ -2,7 +2,6 @@
 
 import { useState, useRef } from "react";
 import Link from "next/link";
-import Image from "next/image";
 import { format } from "date-fns";
 import { updateProject } from "@/app/[companyId]/projects/actions";
 import { useRouter } from "next/navigation";
